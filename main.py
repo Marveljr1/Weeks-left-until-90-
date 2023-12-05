b897eeb298daf33891220448e727ead250c5d563
Keith Williams <kwilliams623@hotmail.com>
2023-12-05 01:27:02 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Weeks left until a person is 90
+print("Welcome to how many weeks do I have left!!!!")
+print("Please enter your age below")
+age = input()
+
+years = 90 - int(age)
+weeks = years * 52
+
+print(f"You have {weeks} weeks left until your 90 years old!")
